5149376d453ea24b1e7076f5ae1e8dbb13b9985f
pra11chit <pratikchitode@gmail.com>
2019-03-20 02:54:03 +0530

    Add files via upload

---
+++ week2/newton_raphson_bisection_square_root.py
@@ -0,0 +1,32 @@
+x=float(input("enter no. whose square root you want to find"))
+low=1.0
+high=x
+guess=(high+low)/2.0
+epsilon=0.001
+steps=0
+
+if x>1:
+    while abs(guess**2-x)>=epsilon:
+        steps+=1
+        if abs(guess**2)<x:
+            low=guess
+        else :
+            high=guess
+        guess=guess-(guess**2-x)/(2*guess)
+        print("low "+str(low)+", high "+str(high)+", guess "+str(guess))
+
+else:
+    low=x
+    high=1
+    guess=(high+low)/2
+    while abs(guess**2-x)>=epsilon:
+        steps+=1
+        if abs(guess**2)>x:
+            high=guess
+        else:
+            low=guess
+        guess=guess-(guess**2-x)/(2*guess)  
+        print("low "+str(low)+", high "+str(high)+", guess "+str(guess))
+
+print("square root of x "+str(guess))   
+print("no. of steps taken "+str(steps)) 
